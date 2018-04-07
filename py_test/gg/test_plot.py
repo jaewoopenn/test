3303d4f954ba84670bb8a78f33e6fa3388d24373
cau <cau@cau-PC>
2018-04-07 22:58:39 +0900

    aaa

---
+++ py_test/gg/test_plot.py
@@ -0,0 +1,17 @@
+'''
+Created on 2015. 12. 11.
+
+@author: cpslab
+'''
+import matplotlib.pyplot as plt
+import matplotlib
+
+if __name__ == '__main__':
+    x=[1,2,3,4]
+    v1=[3,4,2,4]
+    v2=[4,3,2,1]
+    plt.plot(x,v1)
+    plt.plot(x,v2)
+    plt.ylim(0,5)
+    print "hihi"
+    plt.show()
---
+++ py_test/test.py
@@ -0,0 +1,5 @@
+'''
+Created on 2017. 8. 15.
+
+@author: cpslab
+'''
